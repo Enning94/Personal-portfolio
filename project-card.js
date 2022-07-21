87503eac5983013eb9f94ce8f5a4fabda6bc1c88
enning94 <79149176+Enning94@users.noreply.github.com>
2022-07-21 13:48:49 +0000

    Render html using javascript

--- project-card.js
+++ project-card.js
@@ -1,3 +1,4 @@
+const { get } = require("http");
 
 const  project = 
   [  
@@ -72,3 +73,48 @@ const  project =
 }
 getProject(project);
 
+// function popupWindow(){
+//     let html=``;
+//     html+=` <div class='popup-container' id='Popup'>
+//     <div class='popup'>
+
+//       <div class='modal-container'>
+//          <div class='btn-wrapper'>
+//           <button type='button' class='close-btn' data-dismiss='modal'>&times;</button> 
+//           <h4 class='project-title'>Multi Post Stories</h4>
+//         </div>
+         
+//         <div class='technology-list'>
+//           <ul class="tags"> 
+//             <li class="tag-text">css</li>
+//              <li class="tag-text">html</li>
+//              <li class="tag-text">bootstrap</li>
+//              <li class="tag-text">ruby</li>
+//            </ul>
+//         </div>
+//         <div class='modal-img-container'>
+//           <div class='modal-img'>
+//             <img src='./img/popup.png' alt='SnapshootPortfolio'>
+//           </div>
+//           <div class='project-content'>
+//             <div class='supporting-text7'>
+//               Lorem Ipsum is simply dummy text of the printing 
+//               and typesetting industry. Lorem Ipsum has been the industry's 
+//               standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+//                but also the leap into electronic typesetting, remaining essent
+//             </div>
+//             <div class='modal-btn'>
+//               <button type='button' class='bottom-btn' ><a href='${projects[0].livedemo}'>See Live</a><i class='fa fa-external-link' aria-hidden='true'></i></button>
+//               <button type='button' class='bottom-btn'><a href='${projects[0].sourceCode}'>See Source</a><i class='fa fa-github' aria-hidden='true'></i></button>
+//             </div>
+//           </div>
+//         </div>
+//         </div>
+        
+//       </div>
+      
+//     </div>
+//   </div> `
+// }
+// document.querySelector('popup-container').innerHTML=html;
+// popupWindow();
