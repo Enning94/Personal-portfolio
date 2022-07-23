20d58618aa2b9e93722ce640efffccc9eaf34ab4
enning94 <79149176+Enning94@users.noreply.github.com>
2022-07-23 01:32:05 +0000

    update error message

--- form_validation.js
+++ form_validation.js
@@ -1,27 +1,25 @@
-const Fname = document.getElementById("formName");
-const email = document.getElementById("formEmail");
-const textarea = document.getElementById("formMessage");
-const form = document.getElementById("formField");
-const errorElement = document.getElementById("Error");
+const Fname = document.getElementById('formName');
+const email = document.getElementById('formEmail');
+const form = document.getElementById('formField');
+const errorElement = document.getElementById('Error');
 
 const isValidEmail = (email) => {
-  const re =
-    /^([a-z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-z0-9\-]+\.)+))([a-z]{2,4}|[0-9]{1,3})(\]?)$/;
+  const re = /^([a-z0-9_\-.]+)@((\[[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.)|(([a-z0-9-]+\.)+))([a-z]{2,4}|[0-9]{1,3})(]?)$/;
   return re.test(String(email));
 };
 
-form.addEventListener("submit", (e) => {
-  let message = [];
-  if (Fname.value === "" || Fname.value === null) {
-    message.push("Name is required");
+form.addEventListener('submit', (e) => {
+  const message = [];
+  if (Fname.value === '' || Fname.value === null) {
+    message.push('Name is required');
   }
 
   if (!isValidEmail(email.value)) {
-    message.push("Form Unable to submit, Email should be in Lower case");
+    message.push('Form Unable to submit, Email should be in Lower case');
   }
 
   if (message.length > 0) {
     e.preventDefault();
-    errorElement.innerText = message.join(",");
+    errorElement.innerText = message.join(',');
   }
 });
